1ee5f9a83f96c94087f1e607840c736c3a43b8a5
John Ubante <condoti@yahoo.com>
2018-09-24 23:03:36 -0700

    Toy singleton.
    Next:
    - read matrix into Score singleton
    - SM players
    - persistent data file

--- matrix/score.go
+++ matrix/score.go
@@ -1,8 +1,28 @@
 package matrix
 
+import "sync"
+
 // https://medium.com/@MrToBe/the-singleton-object-oriented-design-pattern-in-golang-9f6ce75c21f7
 // http://marcio.io/2015/07/singleton-pattern-in-go/
 type Score struct {
 	filename string
-	matrixMap map[string]map[string]int
+	//matrixMap map[string]map[string]int
+}
+
+var singleton *Score
+var once sync.Once
+
+func (s *Score) GetFilename() string {
+	return s.filename
+}
+
+func (s *Score) SetFilename(fn string) {
+	s.filename = fn
+}
+
+func GetScore() *Score {
+	once.Do(func() {
+		singleton = &Score{"default_filename"}
+	})
+	return singleton
 }
